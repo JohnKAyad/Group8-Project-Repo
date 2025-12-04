cabaa2619629dea2e3782c3dac9123eb9a2db1a6
charleskhu675 <charleskhu675@gmail.com>
2025-12-04 10:49:04 -0800

    Update Hub.h

--- Hub.h
+++ Hub.h
@@ -1,1 +1,27 @@
+#include <string>
+#include "Constants.h"
+using namespace std;
 
+#ifndef HUB_H
+#define HUB_H
+
+class Sensor;
+class DataExport;
+class TelemetryRecord;
+
+class Hub {
+    private:
+        Sensor* sensors[NUM_SENSORS];
+        DataExport* exporters[NUM_EXPORTERS];
+
+        void convertTemp(TelemetryRecord& record);
+        void sendToExporter(const TelemetryRecord& record) const;
+        
+    public:
+        Hub();
+        ~Hub();
+
+        void receive(TelemetryRecord& record);
+        
+};
+#endif
